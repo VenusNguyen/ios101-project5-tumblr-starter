3574e39a0520e7dc8d052996f0afbb3f79431a6b
Venus Nguyen <letn6@uci.edu>
2025-04-02 23:27:40 -0700

    Configured table view

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,16 +6,37 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private var posts: [Post] = []
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create the cell
+        let cell = UITableViewCell()
+
+        // Get the movie-associated table view row
+        let post = posts[indexPath.row]
+
+        // Configure the cell (i.e., update UI elements like labels, image views, etc.)
+        cell.textLabel?.text = post.summary
+
+        // Return the cell for use in the respective table view row
+        return cell
+    }
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
         
         fetchPosts()
     }
-
 
 
     func fetchPosts() {
@@ -48,6 +69,9 @@ class ViewController: UIViewController {
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    
+                    self?.posts = posts
+                    self?.tableView.reloadData()
                 }
 
             } catch {
